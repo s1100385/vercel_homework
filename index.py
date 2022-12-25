252c6516bf04190eb4795c556beaae53293d08dd
Acheng <113333192+s1100385@users.noreply.github.com>
2022-12-25 11:47:24 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("final-4f34e-firebase-adminsdk-coitm-c796101b62.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 app = Flask(__name__)
 
@@ -29,6 +37,34 @@ def welcome():
 @app.route("/about")
 def about():
     return render_template("aboutme.html")
+
+@app.route("/webhook", methods=["POST"])
+def webhook4():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "asktags"):
+        tags =  req.get("queryResult").get("parameters").get("tags")
+        if (tags == "小說"):
+            tags = "輔導級(未滿十二歲之兒童不得觀賞)"
+        elif (tags == "漫畫"):
+            tags = "輔導級(未滿十五歲之人不得觀賞)"
+        info = "您選擇的電影分級是：" + tags + "，相關書籍：\n"
+
+        collection_ref = db.collection("book")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if tags in dict["tags"]:
+                result += "書名：" + dict["bookname"] + "\n"
+                result += "價錢：" + dict["price"] + "\n\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run()
